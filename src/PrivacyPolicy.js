5202f125bfeb27ad3a278167f24b8935fc9234e4
Sarah Weinstein <sweinstein26@gmail.com>
2020-08-07 18:41:23 -0700

    Add more detailed privacy policy

--- src/PrivacyPolicy.js
+++ src/PrivacyPolicy.js
@@ -5,9 +5,68 @@ class PrivacyPolicy extends React.PureComponent {
   render() {
     return (
       <div className="privacy-policy">
-        The purpose of this application is to facilitate a CSV to Google Slides automated transfer of information.
-        <br/><br/>
-        Data provided to the application is not stored in any location, and users must have ownership of the slides that they are exporting data into. (Validated using Google Oauth)
+        <h2>Privacy Policy</h2>
+          This Statement of Privacy applies to
+          https://teaching-scripts.herokuapp.com/ (referred to in this policy as "this site"),
+          and governs data collection and usage. This site is a Google Drive Tooling website.
+          The intent of this site is to facilitate teachers' ability to import form data into
+          Google Slides. By using the site, you consent to the data practices described in this
+          statement.
+        <br/>
+          <h4>Collection of your Personal Information</h4>
+            We do not collect any personal information about you unless you
+            voluntarily provide it to us. However, you may be required to
+            provide certain personal information to us when you elect to use
+            certain products or services available on this site. These may
+            include: (a) registering for an account on our site; (b) sending us an
+            email message. To wit, we will use your information for, but not limited
+            to, communicated with you in relation to services and/or products you
+            have requested from us. We may also gather additional personal or
+            non-personal information in the future.
+          <h4>Sharing Information with Third Parties</h4>
+            This site does not collect a customer list, nor does it sell, rent, or
+            lease a customer lists to third parties.
+            <br/><br/>
+            This site may share data with trusted partners to help perform authentication,
+            send you an email or postal mail, or provide customer support. All such third parties
+            are prohibited from using your personal information except to provide these services,
+            and are required to maintain the confidentiality of your information.
+            <br/><br/>
+            This site may disclose your personal information, without notice, if required to do so
+            by law or in the good faith belief that such action is necessary to: (a) conform to the
+            edicts of the law or comply with legal process; (b) protect and defent the rights or property
+            of the site; and/or (c) act under exigent circumstances to protect the personal safety of users,
+            or the public
+          <h4>Automatically Collected Information</h4>
+            Information about your computer hardware and software may be automatically collected by this site.
+            This information can include: your IP address, browser type, domain names, access times, and referring
+            website addresses. This information is used for the operation of the service, to maintain quality
+            of the service, and to provide general statistics regarding use of this site.
+          <h4>Security of your Personal Information</h4>
+            This site secures your personal information from unauthorized access, use, or disclosure.
+            This site does not implement its own authentication, but rather relies on that of Google OAuth2.
+            <br/><br/>
+            We strive to take appropriate security measures to protect against unauthorized access to or alteration
+            of your personal information. Unfortunately, no data transmission over the Internet or any wireless network
+            can be guarenteed to be 100% secure. As a result, while we strive to protect your personal information,
+            you acknowledge that: (a) there are security and privacy limitations inherent to the Internet which are
+            beyond our control; (b) security, integrity, and privacy of any and all information and data exchanged between
+            you and us through this site cannot be guaranteed.
+          <h4>Children Under Thirteen</h4>
+            This site does not knowingly collect personally identifiable information from children under the age
+            of thirteen. If you are under the age of thirteen, you must ask your parent or guardian for permission to
+            use this website.
+          <h4>Changes to this Statement</h4>
+            We reserve the right to change this Privacy Policy from time to time. We will notify you about significant
+            changes in the way we treat personal information by placing a notice on this site, and/or by updating any
+            privacy information on this page. Your continued use of thie site and/or services available through this site
+            after such modifications will constitute your: (a) acknowledgement of the modified Privacy Policy; and (b)
+            agreement to abide and be bound by that Policy.
+          <h4>Contact Information</h4>
+            We welcome your questions or comments regarding this Statement of Privacy. Please contact Sarah Weinstein
+            at sweinstein22@yahoo.com.
+          <br/><br/><br/>
+          Effective as of August 07, 2020
       </div>
     )
   }
